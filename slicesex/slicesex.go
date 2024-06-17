4644b9a5f5d4252008a1d401d51de745c2849c22
Igor Karatayev <igor.cwer@gmail.com>
2024-06-17 17:15:22 +0300

    adopt xiter usage

--- slicesex/slicesex.go
+++ slicesex/slicesex.go
@@ -3,7 +3,7 @@ package slicesex
 import (
 	"slices"
 
-	"golang.org/x/exp/constraints"
+	"github.com/DeedleFake/xiter"
 )
 
 func BinarySearchInsert[S ~[]E, E any](x S, item E, cmp func(E, E) int) S {
@@ -11,24 +11,7 @@ func BinarySearchInsert[S ~[]E, E any](x S, item E, cmp func(E, E) int) S {
 	return slices.Insert(x, i, item)
 }
 
-func Accumulate[S ~[]E, E constraints.Integer | constraints.Float | constraints.Complex](x S) E {
-	var r E
-	for _, t := range x {
-		r += t
-	}
-	return r
-}
-
-func Any[S ~[]E, E any](x S, fn func(E) bool) bool {
-	for _, t := range x {
-		if fn(t) {
-			return true
-		}
-	}
-	return false
-}
-
-func Select[S ~[]E, E, V any](x S, fn func(E) V) []V {
+func Transform[S ~[]E, E, V any](x S, fn func(E) V) []V {
 	result := make([]V, len(x))
 	for i, t := range x {
 		result[i] = fn(t)
@@ -36,22 +19,6 @@ func Select[S ~[]E, E, V any](x S, fn func(E) V) []V {
 	return result
 }
 
-func WhereSelect[S ~[]E, E, V any](x S, whereFn func(E) bool, selectFn func(E) V) []V {
-	i := 0
-	result := make([]V, 0, len(x))
-	for _, t := range x {
-		if whereFn(t) {
-			result[i] = selectFn(t)
-			i++
-		}
-	}
-	return result
-}
-
-func Range(start int, count int) []int {
-	nums := make([]int, count)
-	for i := range nums {
-		nums[i] = start + i
-	}
-	return nums
+func Range(start int, count int) xiter.Seq[int] {
+	return xiter.Limit(xiter.Generate(start, 1), count)
 }
