e5510c859bd0b2101e99e981ce945dfbe7c52757
Igor Karatayev <igor.cwer@gmail.com>
2025-11-05 17:26:02 +0300

    string processing return error if fields not found

--- stringsex/template.go
+++ stringsex/template.go
@@ -5,34 +5,43 @@ import (
 )
 
 // ProcessStringAny обрабатывает строку, заменяя шаблоны ${key} на значения из mapping
-func ProcessStringAny(input string, mapping map[string]any) string {
+func ProcessStringAny(input string, mapping map[string]any) (result string, ok bool) {
 	// Регулярное выражение для поиска шаблонов ${key}
 	re := regexp.MustCompile(`\${([^}]*)}`)
 
+	var toString func(v any) string
+	toString = func(v any) string {
+		switch v := v.(type) {
+		case []any:
+			return "{" + JoinSeq(func(yield func(string) bool) {
+				for _, i := range v {
+					if !yield(toString(i)) {
+						return
+					}
+				}
+			}, ", ") + "}"
+		case string:
+			return v
+		}
+
+		return ""
+	}
+
 	// Функция замены
-	result := re.ReplaceAllStringFunc(input, func(match string) string {
+	ok = true
+	result = re.ReplaceAllStringFunc(input, func(match string) string {
 		// Извлекаем ключ из шаблона ${key}
 		key := match[2 : len(match)-1] // Убираем ${ и }
 
 		// Ищем значение в мапе
 		if value, exists := mapping[key]; exists {
-			switch v := value.(type) {
-			case []any:
-				return JoinSeq(func(yield func(string) bool) {
-					for _, i := range v {
-						if !yield(i.(string)) {
-							return
-						}
-					}
-				}, ", ")
-			case string:
-				return v
-			}
+			return toString(value)
 		}
 
 		// Если ключ не найден, возвращаем оригинальный шаблон
+		ok = false
 		return match
 	})
 
-	return result
+	return
 }
