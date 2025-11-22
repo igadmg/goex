129e6bd3f2fc1fa50ccbb0396e9292544bf7a688
Igor Karatayev <igor.cwer@gmail.com>
2025-11-22 19:16:02 +0300

    remove unused CopyFile function from osex package use github.com/otiai10/copy instead

--- osex/osex.go
+++ osex/osex.go
@@ -5,23 +5,6 @@ import (
 	"os"
 )
 
-func CopyFile(source, destination string) error {
-	r, err := os.Open(source)
-	if err != nil {
-		return err
-	}
-	defer r.Close()
-
-	w, err := os.Create(destination)
-	if err != nil {
-		return err
-	}
-	defer w.Close()
-
-	w.ReadFrom(r)
-	return nil
-}
-
 // isDirectory reports whether the named file is a directory.
 func IsDirectory(name string) bool {
 	info, err := os.Stat(name)
