090c970308533974a0ddaae2f99761574368249b
Igor Karatayev <igor.cwer@gmail.com>
2026-03-05 00:37:17 +0300

    Cancellabe and CancellabeWithTimeout

--- syncex/waitgroup.go
+++ syncex/waitgroup.go
@@ -1,7 +1,9 @@
 package syncex
 
 import (
+	"context"
 	"sync"
+	"time"
 )
 
 // WaitGroupGo runs multiple functions concurrently and calls waitFn when all are done.
@@ -22,4 +24,36 @@ func WaitGroupGo(waitFn func(), fn ...func()) {
 		wg.Wait()
 		waitFn()
 	}()
+}
+
+func Cancellabe(ctx context.Context, fn func(context.Context) bool) func() {
+	return func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				if !fn(ctx) {
+					return
+				}
+			}
+		}
+	}
+}
+
+func CancellabeWithTimeout(ctx context.Context, timeout time.Duration, fn func(context.Context) bool) func() {
+	return func() {
+		for {
+			select {
+			case <-time.After(timeout):
+				return
+			case <-ctx.Done():
+				return
+			default:
+				if !fn(ctx) {
+					return
+				}
+			}
+		}
+	}
 }
