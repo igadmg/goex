03be130992aebaab58402de760c0d675cdc9b97b
Igor Karatayev <igor.cwer@gmail.com>
2026-04-13 01:17:54 +0300

    GetOutboundIP

--- netex/netex.go
+++ netex/netex.go
@@ -1,20 +1,36 @@
 package netex
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
+
+func GetOutboundIP() (net.IP, error) {
+	// 8.8.8.8:80 doesn't need to be reachable or even exist.
+	// This just helps Go determine the local IP for external traffic.
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return net.IP{}, err
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP, nil
+}
 
 // GetLocalIP returns the non loopback local IP of the host
-func GetLocalIP() net.IP {
+func GetLocalIP() (net.IP, error) {
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
-		return net.IP{}
+		return net.IP{}, err
 	}
 	for _, address := range addrs {
 		// check the address type and if it is not a loopback the display it
 		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
 			if ipnet.IP.To4() != nil {
-				return ipnet.IP
+				return ipnet.IP, nil
 			}
 		}
 	}
-	return net.IP{}
+	return net.IP{}, fmt.Errorf("no external interface found")
 }
