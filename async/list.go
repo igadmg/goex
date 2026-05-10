7170f59180f30c4b1687b4bc02a0196e2e3f00fc
Igor Karatayev <igor.cwer@gmail.com>
2026-05-10 18:08:41 +0300

    templated sync.Map + asyn.List ListNotifier

---
+++ async/list.go
@@ -0,0 +1,53 @@
+package async
+
+import (
+	"iter"
+	"slices"
+)
+
+type List[T any] struct {
+	Items     iter.Seq[T]
+	notifiers []*ListNotifier[T]
+}
+
+type ListNotifier[T any] struct {
+	list      *List[T]
+	OnAdded   func(v T)
+	OnRemoved func(v T)
+}
+
+func (l *List[T]) Subscribe() *ListNotifier[T] {
+	ln := &ListNotifier[T]{list: l}
+	l.notifiers = append(l.notifiers, ln)
+	return ln
+}
+
+func (l *List[T]) Unsubscribe(ln *ListNotifier[T]) {
+	l.notifiers = slices.DeleteFunc(l.notifiers, func(n *ListNotifier[T]) bool {
+		return n == ln
+	})
+}
+
+func (l *List[T]) Add(v T) {
+	for _, n := range l.notifiers {
+		if n.OnAdded != nil {
+			n.OnAdded(v)
+		}
+	}
+}
+
+func (l *List[T]) Remove(v T) {
+	for _, n := range l.notifiers {
+		if n.OnRemoved != nil {
+			n.OnRemoved(v)
+		}
+	}
+}
+
+func (l *ListNotifier[T]) Defer() {
+	l.list.Unsubscribe(l)
+}
+
+func (l *ListNotifier[T]) Unsubscribe() {
+	l.list.Unsubscribe(l)
+}
